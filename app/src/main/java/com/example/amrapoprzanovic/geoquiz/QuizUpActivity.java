8a2b835a5e949d516b78c9955b897beeaa470783
Amra <amrapop@gmail.com>
2015-04-02 16:47:44 +0200

    fourth day and new Activity

--- app/src/main/java/com/example/amrapoprzanovic/geoquiz/QuizUpActivity.java
+++ app/src/main/java/com/example/amrapoprzanovic/geoquiz/QuizUpActivity.java
@@ -1,7 +1,6 @@
 package com.example.amrapoprzanovic.geoquiz;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,18 +16,34 @@ public class QuizUpActivity extends ActionBarActivity {
 
     private final String TAG = "QuizActivity";
     public static final String INTENT_KEY_MESSAGE= "message_key";
+
+    public static final String INTENT_KEY_CORRECT_ANSWER= "bitcamp.geoQuizUp.correct_answer";
+    public static final String INTENT_KEY_TOTAL_QUESTION= "bitcamp.geoQuizUp.total_question";
+    public static final String INTENT_KEY_CURRENT_QUESTION= "bitcamp.geoQuizUp.current_key";
+    public static final String INTENT_KEY_USERNAME= "bitcamp.geoQuizUp.username";
+
+
+    private static final String BUNDLE_KEY_CORRECT_INDEX ="bitcamp.geoQuizUp.current_key";
+    private static final String BUNDLE_KEY_CORRECT_ANSWER ="bitcamp.geoQuizUp.current_answer";
+    private static final int INPUT_REQUEST_CODE=200;
+
     public static  int correct= 0;
-    public static Integer correctAnswer= 0;
+    public static int correctAnswer= 0;
     public static int incorrect = 0;
+
+    public  int mCorrectAnswer= 0;
 
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mStatsButton;
+    private Button mInputButton;
     private TextView mQuestionText;
 
+    private String mUsername=null;
 
 
     private int mCurrentIndex=0;
+
     private Question[] mQuestions = new Question[]{
             new Question(R.string.question_1, true),
             new Question(R.string.question_2, false),
@@ -45,6 +60,11 @@ public class QuizUpActivity extends ActionBarActivity {
         setContentView(R.layout.activity_quiz_up);
 
         Log.d(TAG, "onCreate Strated");
+
+        if(savedInstanceState!= null){
+            mCurrentIndex = savedInstanceState.getInt(BUNDLE_KEY_CORRECT_INDEX);
+            mCorrectAnswer = savedInstanceState.getInt(BUNDLE_KEY_CORRECT_ANSWER);
+        }
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener( new View.OnClickListener(){
@@ -69,19 +89,32 @@ public class QuizUpActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                Intent statsIntent = new Intent(QuizUpActivity.this, StatsActivity.class);
-                statsIntent.putExtra(INTENT_KEY_MESSAGE, "Broj tacnih odgovra je: " + (correct*100)/(correct+incorrect) + "%");
+                statsIntent.putExtra(INTENT_KEY_MESSAGE, "Broj tacnih odgovora je: " + (correct*100)/(correct+incorrect) + "%");
                 startActivity(statsIntent);
+            }
+        });
+
+        mInputButton = (Button)findViewById(R.id.set_name_button);
+        mInputButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent inputIntent = new Intent(QuizUpActivity.this, InputActivity.class);
+                intputIntent.putExtra(INTENT_KEY_USERNAME, mUsername);
+                startActivity(inputIntent, INPUT_REQUEST_CODE);
             }
         });
 
         mQuestionText = (TextView) findViewById(R.id.question_text);
 //        mQuestionText.setText(mQuestions[mCurrentIndex].getQuestionId());
+
         updateQuestionText();
 
 
     }
 
     private void updateQuestionText(){
+//       mCurrentIndex= (mCurrentIndex +1) % mQuestionText.length();
         mQuestionText.setText(mQuestions[mCurrentIndex].getQuestionId());
     }
 
@@ -103,6 +136,23 @@ public class QuizUpActivity extends ActionBarActivity {
         Toast.makeText(QuizUpActivity.this, toastText, Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outputBundle){
+        super.onSaveInstanceState(outputBundle);
+        outputBundle.putInt(BUNDLE_KEY_CORRECT_ANSWER, mCorrectAnswer);
+        outputBundle.putInt(BUNDLE_KEY_CORRECT_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        Log.d(TAG, "Activity result " + requestCode + " " + resultCode);
+        if(requestCode == INPUT_REQUEST_CODE){
+            if(resultCode==RESULT_OK){
+                mUsername = data.getStringExtra(INTENT_KEY_USERNAME);
+                updateQuestionText();
+            }
+        }
+    }
 
     @Override
     protected void onPause() {
